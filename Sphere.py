eb04e26e9cabf228499f413132e437053e6c6d13
nunziati <giacomo.nunziati.0@gmail.com>
2023-02-02 18:06:16 +0100

    Sphere.py refactored

--- Sphere.py
+++ Sphere.py
@@ -1,20 +1,19 @@
 from Solid import Solid
 import numpy as np
 
+from numpy.typing import NDArray
+
 class Sphere(Solid):
-    def __init__(self, center, radius: float):
+    def __init__(self, center: NDArray[np.float32], radius: float):
         if center.shape != (3,):
-            raise ValueError("Sphere center must be a 3D vector")
+            raise ValueError("must be numpy.ndarray objects with shape (3,)")
         if radius <= 0:
             raise ValueError("Sphere radius must be positive")
-        self.center = center
-        self.radius = radius
+        self.center: NDArray[np.float32] = center
+        self.radius: float = radius
 
-    def is_inside(self, points):
+    def is_inside(self, points: NDArray[np.float32]) -> NDArray[np.float32]:
         return ((points - self.center) ** 2).sum(axis=1) <= self.radius ** 2
     
-    def get_extreme_coordinates(self):
+    def get_extreme_coordinates(self) -> NDArray[np.float32]:
         return np.stack(((self.center - self.radius), (self.center + self.radius)), axis=-1)
-
-    def get_surface_points(self, phi_values=16, theta_values=8):
-        pass
