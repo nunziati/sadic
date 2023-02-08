4b1c4c8cce6d5c816f7edd317ec7adc27e4c7f0f
nunziati <giacomo.nunziati.0@gmail.com>
2023-02-08 11:57:48 +0100

    sadic functions for sphere reduction

--- sadic.py
+++ sadic.py
@@ -6,7 +6,7 @@ from numpy.typing import NDArray
 import numpy as np
 
 default_quantizer_class = RegularStepsSphericalQuantizer
-default_quantizer_kwargs = {"rho_steps_number": , "theta_steps_number": 360, "phi_steps_number": 180}
+default_quantizer_kwargs = {"rho_steps_number": 10, "theta_steps_number": 360, "phi_steps_number": 180}
 
 """def reduce_multisphere(multisphere: Multisphere, quantizer: SphericalQuantizer):
     max_radii = []
@@ -29,7 +29,14 @@ default_quantizer_kwargs = {"rho_steps_number": , "theta_steps_number": 360, "ph
     print(max_radii)
     print(max(max_radii))"""
 
-def find_candidate_max_radius_points(multisphere: Multisphere, quantizer_arg: SphericalQuantizer | None = None, min_radius: float = 1.52, multiplier: float = 2) -> tuple[NDArray[np.float32], float]:
+def find_candidate_max_radius_points(
+        multisphere: Multisphere,
+        quantizer_arg: SphericalQuantizer | None = None,
+        min_radius: float = 1.52,
+        multiplier: float = 2,
+        exclude_points: NDArray[np.int32] = np.array([], dtype=np.int32)
+        ) -> tuple[NDArray[np.float32], float]:
+
     centers: NDArray[np.float32] = multisphere.get_all_centers()
     max_radius: float = min_radius
     
@@ -38,6 +45,9 @@ def find_candidate_max_radius_points(multisphere: Multisphere, quantizer_arg: Sp
     max_radius_points: list[int] = [0]
 
     for idx in range(centers.shape[0]):
+        if idx in exclude_points:
+            continue
+
         radius: float = min_radius * multiplier
         sphere: Sphere = Sphere(centers[idx], radius)
         points: NDArray[np.float32] = quantizer.get_surface_points(sphere)
@@ -60,12 +70,20 @@ def find_candidate_max_radius_points(multisphere: Multisphere, quantizer_arg: Sp
 
     return np.array(max_radius_points), max_radius
 
-def find_max_radius_point(multisphere: Multisphere, quantizer_arg: SphericalQuantizer | None = None, min_radius: float = 1.52, multiplier: float = 2, bisection_threshold: float = 1) -> tuple[NDArray[np.int32], int]:
+def find_max_radius_point(
+        multisphere: Multisphere,
+        quantizer_arg: SphericalQuantizer | None = None,
+        min_radius: float = 1.52,
+        multiplier: float = 2,
+        exclude_points: NDArray[np.int32] = np.array([], dtype=np.int32),
+        bisection_threshold: float = 1
+        ) -> tuple[NDArray[np.int32], np.int32]:
+    
     quantizer: SphericalQuantizer = default_quantizer_class(**default_quantizer_kwargs) if quantizer_arg is None else quantizer_arg
 
-    candidate_max_radius_points, max_radius = find_candidate_max_radius_points(multisphere, quantizer, min_radius, multiplier)
+    candidate_max_radius_points, max_radius = find_candidate_max_radius_points(multisphere, quantizer, min_radius, multiplier=multiplier, exclude_points=exclude_points)
     
-    max_radii: NDArray[np.int32] = np.empty(candidate_max_radius_points.shape[0], dtype=np.int32)
+    max_radii: NDArray[np.float32] = np.empty(candidate_max_radius_points.shape[0], dtype=np.float32)
 
     for idx, candidate in enumerate(candidate_max_radius_points):
         a = max_radius 
@@ -82,6 +100,23 @@ def find_max_radius_point(multisphere: Multisphere, quantizer_arg: SphericalQuan
         max_radii[idx] = ((a + b) / 2)
 
     return candidate_max_radius_points[np.argmax(max_radii)], np.max(max_radii)
+
+def reduce_multisphere_step(
+        multisphere: Multisphere,
+        quantizer_arg: SphericalQuantizer | None = None,
+        min_radius: float = 1.52,
+        multiplier: float = 2,
+        exclude_points: NDArray[np.int32] = np.array([], dtype=np.int32),
+        bisection_threshold: float = 1,
+        include_radius: bool = True
+        ) -> Multisphere:
+    
+    quantizer: SphericalQuantizer = default_quantizer_class(**default_quantizer_kwargs) if quantizer_arg is None else quantizer_arg
+
+    max_radius_point, max_radius = find_max_radius_point(multisphere, quantizer, min_radius, multiplier, exclude_points, bisection_threshold)
+
+    
+    return Multisphere(multisphere.get_all_centers(), multisphere.get_all_radii(), max_radius_point, max_radius)
     
 
 """def find_reference_radius(multisphere, phi_values, theta_values):
