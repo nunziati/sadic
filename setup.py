cc4ef4c8a954f118777d689b4b9eeb751431399a
nunziati <giacomo.nunziati.0@gmail.com>
2023-07-05 12:41:27 +0200

    format setup.py

--- setup.py
+++ setup.py
@@ -1,48 +1,53 @@
-from setuptools import setup
+r"""setup.py"""
+
 import os
 
+from setuptools import setup
+
+
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    r"""Reads a file and returns its content as a string."""
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding="utf-8").read()
+
 
 def get_version(rel_path):
+    r"""Gets the version of the package from the __init__ file."""
     for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
+        if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-    
-setup(
-    name='sadic',
-    version=get_version("sadic/__init__.py"),    
-    description='Reimplementation as a python package of the software for Simple Atom Depth Index Calculator (SADIC)',
-    url='https://github.com/nunziati/sadic',
-    author='Giacomo Nunziati',
-    author_email='giacomo.nunziati.0@gmail.com',
-    license='GNU General Public License v3.0',
-    keywords = "protein atom depth",
-    long_description=read('README.md'),
-    packages=['sadic', 'sadic.algorithm', 'sadic.quantizer', 'sadic.utils', 'sadic.solid', 'sadic.pdb'],
-    install_requires=[
-        'numpy',
-        'biopandas',
-        'biopython',
-        'tqdm',
-        'matplotlib'
-    ],
 
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="sadic",
+    version=get_version("sadic/__init__.py"),
+    description="Reimplementation as a python package of the software for Simple Atom Depth Index "
+    "Calculator (SADIC)",
+    url="https://github.com/nunziati/sadic",
+    author="Giacomo Nunziati",
+    author_email="giacomo.nunziati.0@gmail.com",
+    license="GNU General Public License v3.0",
+    keywords="protein atom depth",
+    long_description=read("README.md"),
+    packages=[
+        "sadic",
+        "sadic.algorithm",
+        "sadic.quantizer",
+        "sadic.utils",
+        "sadic.solid",
+        "sadic.pdb",
+    ],
+    install_requires=["numpy", "scipy", "biopandas", "biopython", "tqdm", "matplotlib"],
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Natural Language :: Italian',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Scientific/Engineering :: Bio-Informatics'
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
 )
